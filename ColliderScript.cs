6cc4dd4cb131d97ac59b8750ca6bee05f3d95307
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-10-10 22:24:09 -0600

    Collider script template for escape room project

---
+++ ColliderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehavior
+{
+	void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "TrackableName")
+        {
+            Debug.Log("Define collision behavior here.");
+        }
+    }
+}
